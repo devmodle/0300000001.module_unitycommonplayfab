f7c24fc1694daa1932db759ccf5953adf5dbc8a0
dante <are2341@nate.com>
2022-05-11 23:01:27 +0900

    [이상동] Package 설정 파일 추가

---
+++ Scripts/Runtime/Manager/CBackendManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if BACKEND_MODULE_ENABLE
+/** 백엔드 관리자 */
+public partial class CBackendManager : CSingleton<CBackendManager> {
+
+}
+#endif			// #if BACKEND_MODULE_ENABLE

16698fc1a03c53d560f0490bdf463ef0a676f71b
sd.lee <sd.lee@ninetap.com>
2022-05-30 19:23:24 +0900

    [이상동] 서브 모듈 갱신

---
+++ Scripts/Runtime/Manager/CPlayfabManager+Analytics.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if PLAYFAB_MODULE_ENABLE
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.SharedModels;
+
+/** 플레이 팹 관리자 - 분석 */
+public partial class CPlayfabManager : CSingleton<CPlayfabManager> {
+	#region 함수
+	/** 로그를 전송한다 */
+	public void SendLog(string a_oName, Dictionary<string, object> a_oDataDict) {
+		CFunc.ShowLog($"CPlayfabManager.SendLog: {a_oName}, {a_oDataDict}", KCDefine.B_LOG_COLOR_PLUGIN);
+
+#if ((UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE) && PLAYFAB_ANALYTICS_ENABLE) && (ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD)
+		// 로그인 되었을 경우
+		if(this.IsInit && this.IsLogin) {
+			PlayFabClientAPI.WritePlayerEvent(new WriteClientPlayerEventRequest() {
+				EventName = a_oName, Body = a_oDataDict
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.SEND_LOG, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.SEND_LOG, a_oError));
+		}
+#endif			// #if ((UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE) && PLAYFAB_ANALYTICS_ENABLE) && (ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD)
+	}
+
+	/** 앱 로그를 전송한다 */
+	public void SendAppLog(string a_oName, Dictionary<string, object> a_oDataDict) {
+		CFunc.ShowLog($"CPlayfabManager.SendAppLog: {a_oName}, {a_oDataDict}", KCDefine.B_LOG_COLOR_PLUGIN);
+
+		// 로그인 되었을 경우
+		if(this.IsInit && this.IsLogin) {
+			PlayFabClientAPI.WriteTitleEvent(new WriteTitleEventRequest() {
+				EventName = a_oName, Body = a_oDataDict
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.SEND_APP_LOG, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.SEND_APP_LOG, a_oError));
+		}
+	}
+
+	/** 캐릭터 로그를 전송한다 */
+	public void SendCharacterLog(string a_oName, string a_oCharacterID, Dictionary<string, object> a_oDataDict) {
+		CFunc.ShowLog($"CPlayfabManager.SendCharacterLog: {a_oName}, {a_oCharacterID}, {a_oDataDict}", KCDefine.B_LOG_COLOR_PLUGIN);
+
+#if ((UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE) && PLAYFAB_ANALYTICS_ENABLE) && (ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD)
+		// 로그인 되었을 경우
+		if(this.IsInit && this.IsLogin) {
+			PlayFabClientAPI.WriteCharacterEvent(new WriteClientCharacterEventRequest() {
+				EventName = a_oName, CharacterId = a_oCharacterID, Body = a_oDataDict
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.SEND_CHARACTER_LOG, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.SEND_CHARACTER_LOG, a_oError));
+		}
+#endif			// #if ((UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE) && PLAYFAB_ANALYTICS_ENABLE) && (ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD)
+	}
+	#endregion			// 함수
+
+	#region 조건부 함수
+#if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+	/** 로그 전송 응답을 처리한다 */
+	private void HandleSendLogResponse(PlayFabResultCommon a_oResult, bool a_bIsSuccess) {
+		CFunc.ShowLog($"CPlayfabManager.HandleSendLogResponse: {a_bIsSuccess}", KCDefine.B_LOG_COLOR_PLUGIN);
+	}
+
+	/** 앱 로그 전송 응답을 처리한다 */
+	private void HandleSendAppLogResponse(PlayFabResultCommon a_oResult, bool a_bIsSuccess) {
+		CFunc.ShowLog($"CPlayfabManager.HandleSendAppLogResponse: {a_bIsSuccess}", KCDefine.B_LOG_COLOR_PLUGIN);
+	}
+
+	/** 캐릭터 로그 전송 응답을 처리한다 */
+	private void HandleSendCharacterLogResponse(PlayFabResultCommon a_oResult, bool a_bIsSuccess) {
+		CFunc.ShowLog($"CPlayfabManager.HandleSendCharacterLogResponse: {a_bIsSuccess}", KCDefine.B_LOG_COLOR_PLUGIN);
+	}
+#endif			// #if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+	#endregion			// 조건부 함수
+}
+#endif			// #if PLAYFAB_MODULE_ENABLE
---
+++ Scripts/Runtime/Manager/CPlayfabManager+Auth.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if PLAYFAB_MODULE_ENABLE
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.SharedModels;
+
+/** 플레이 팹 관리자 - 인증 */
+public partial class CPlayfabManager : CSingleton<CPlayfabManager> {
+	#region 함수
+	/** 로그인을 처리한다 */
+	public void Login(string a_oDeviceID, System.Action<CPlayfabManager, bool> a_oCallback) {
+		CFunc.ShowLog($"CPlayfabManager.Login: {a_oDeviceID}", KCDefine.B_LOG_COLOR_PLUGIN);
+		CAccess.Assert(a_oDeviceID.ExIsValid());
+
+#if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+		// 초기화 되었을 경우
+		if(!this.IsInit || this.IsLogin) {
+			CFunc.Invoke(ref a_oCallback, this, this.IsLogin);
+		} else {
+			m_oCallbackDict01.ExReplaceVal(EPlayfabCallback.LOGIN, a_oCallback);
+			
+#if !UNITY_EDITOR && UNITY_IOS
+			PlayFabClientAPI.LoginWithIOSDeviceID(new LoginWithIOSDeviceIDRequest() {
+				CreateAccount = true, DeviceId = a_oDeviceID, DeviceModel = SystemInfo.deviceModel, OS = SystemInfo.operatingSystem, TitleId = PlayFabSettings.staticSettings.TitleId
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.LOGIN, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.LOGIN, a_oError));
+#elif !UNITY_EDITOR && UNITY_ANDROID
+			PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest() {
+				CreateAccount = true, AndroidDeviceId = a_oDeviceID, AndroidDevice = SystemInfo.deviceModel, OS = SystemInfo.operatingSystem, TitleId = PlayFabSettings.staticSettings.TitleId
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.LOGIN, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.LOGIN, a_oError));
+#else
+			PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest() {
+				CreateAccount = true, CustomId = a_oDeviceID, TitleId = PlayFabSettings.staticSettings.TitleId
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.LOGIN, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.LOGIN, a_oError));
+#endif			// #if !UNITY_EDITOR && UNITY_IOS
+		}
+#else
+		CFunc.Invoke(ref a_oCallback, this, false);
+#endif			// #if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+	}
+
+	/** 애플 로그인을 처리한다 */
+	public void LoginWithApple(string a_oUserID, string a_oIDToken, System.Action<CPlayfabManager, bool> a_oCallback) {
+		CFunc.ShowLog($"CPlayfabManager.LoginWithApple: {a_oUserID}, {a_oIDToken}", KCDefine.B_LOG_COLOR_PLUGIN);
+		CAccess.Assert(a_oUserID.ExIsValid() && a_oIDToken.ExIsValid());
+
+#if UNITY_IOS && APPLE_LOGIN_ENABLE
+		// 초기화 되었을 경우
+		if(!this.IsInit || this.IsLogin) {
+			CFunc.Invoke(ref a_oCallback, this, this.IsLogin);
+		} else {
+			m_oCallbackDict01.ExReplaceVal(EPlayfabCallback.LOGIN, a_oCallback);
+
+			PlayFabClientAPI.LoginWithApple(new LoginWithAppleRequest() {
+				CreateAccount = true, IdentityToken = a_oIDToken, TitleId = PlayFabSettings.staticSettings.TitleId
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.LOGIN, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.LOGIN, a_oError));
+		}
+#else
+		CFunc.Invoke(ref a_oCallback, this, false);
+#endif			// #if UNITY_IOS && APPLE_LOGIN_ENABLE
+	}
+
+	/** 페이스 북 로그인을 처리한다 */
+	public void LoginWithFacebook(string a_oAccessToken, System.Action<CPlayfabManager, bool> a_oCallback) {
+		CFunc.ShowLog($"CPlayfabManager.LoginWithFacebook: {a_oAccessToken}", KCDefine.B_LOG_COLOR_PLUGIN);
+		CAccess.Assert(a_oAccessToken.ExIsValid());
+
+#if (UNITY_IOS || UNITY_ANDROID) && FACEBOOK_MODULE_ENABLE
+		// 초기화 되었을 경우
+		if(!this.IsInit || this.IsLogin) {
+			CFunc.Invoke(ref a_oCallback, this, this.IsLogin);
+		} else {
+			m_oCallbackDict01.ExReplaceVal(EPlayfabCallback.LOGIN, a_oCallback);
+
+			PlayFabClientAPI.LoginWithFacebook(new LoginWithFacebookRequest() {
+				CreateAccount = true, AccessToken = a_oAccessToken, TitleId = PlayFabSettings.staticSettings.TitleId
+			}, (a_oResponse) => this.OnReceiveResponse(EPlayfabCallback.LOGIN, a_oResponse), (a_oError) => this.OnReceiveFailResponse(EPlayfabCallback.LOGIN, a_oError));
+		}
+#else
+		CFunc.Invoke(ref a_oCallback, this, false);
+#endif			// #if (UNITY_IOS || UNITY_ANDROID) && FACEBOOK_MODULE_ENABLE
+	}
+	
+	/** 로그아웃을 처리한다 */
+	public void Logout(System.Action<CPlayfabManager> a_oCallback) {
+		CFunc.ShowLog("CPlayfabManager.Logout", KCDefine.B_LOG_COLOR_PLUGIN);
+
+#if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+		// 로그인 되었을 경우
+		if(this.IsInit && this.IsLogin) {
+			PlayFabSettings.staticPlayer.ClientSessionTicket = string.Empty;
+		}
+#endif			// #if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+
+		CScheduleManager.Inst.AddCallback(KCDefine.U_KEY_PLAYFAB_M_HANDLE_LOGOUT_RESPONSE_CALLBACK, () => CFunc.Invoke(ref a_oCallback, this));
+	}
+	#endregion			// 함수
+
+	#region 조건부 함수
+#if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+	/** 로그인 응답을 처리한다 */
+	private void HandleLoginResponse(PlayFabResultCommon a_oResult, bool a_bIsSuccess) {
+		CFunc.ShowLog($"CPlayfabManager.HandleLoginResponse: {a_bIsSuccess}", KCDefine.B_LOG_COLOR_PLUGIN);
+		CScheduleManager.Inst.AddCallback(KCDefine.U_KEY_PLAYFAB_M_HANDLE_LOGIN_RESPONSE_CALLBACK, () => this.UserID = a_bIsSuccess ? (a_oResult as LoginResult).PlayFabId : string.Empty);
+	}
+#endif			// #if UNITY_IOS || UNITY_ANDROID || UNITY_STANDALONE
+	#endregion			// 조건부 함수
+}
+#endif			// #if PLAYFAB_MODULE_ENABLE

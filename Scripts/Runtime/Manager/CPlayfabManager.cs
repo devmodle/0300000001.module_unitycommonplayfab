08b4623eacfa41f683e0fb36f56cbcf94ff0ed18
sd.lee <sd.lee@ninetap.com>
2022-09-05 23:50:34 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CPlayfabManager.cs
+++ Scripts/Runtime/Manager/CPlayfabManager.cs
@@ -189,9 +189,9 @@ public partial class CPlayfabManager : CSingleton<CPlayfabManager> {
 
 	#region 클래스 함수
 	/** 매개 변수를 생성한다 */
-	public static STParams MakeParams(Dictionary<ECallback, System.Action<CPlayfabManager, bool>> a_oCallbackDict) {
+	public static STParams MakeParams(Dictionary<ECallback, System.Action<CPlayfabManager, bool>> a_oCallbackDict = null) {
 		return new STParams() {
-			m_oCallbackDict = a_oCallbackDict
+			m_oCallbackDict = a_oCallbackDict ?? Dictionary<ECallback, System.Action<CPlayfabManager, bool>>()
 		};
 	}
 	#endregion			// 클래스 함수
